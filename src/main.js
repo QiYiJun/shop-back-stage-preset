7b75c56d94cdca80cf5a95de376f8b4bed2db57e
QiYiJun <1497055242@qq.com>
2023-03-14 15:22:15 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import axios from 'axios'
 import './plugins/element.js'
 import 'assets/css/base.css'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 Vue.config.productionTip = false
 
@@ -15,8 +19,25 @@ axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
 Vue.prototype.$http = axios
+
 Vue.component('tree-table', TreeTable)
+
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
